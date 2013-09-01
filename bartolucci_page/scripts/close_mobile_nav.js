f9cfb2380661de3fd8dd0d6edcbf4b8493b29c20
hillmandj <hillmandj@gmail.com>
2013-09-01 14:17:49 -0400

    modifications following class on 8/29/2013

--- bartolucci_page/scripts/close_mobile_nav.js
+++ bartolucci_page/scripts/close_mobile_nav.js
@@ -3,10 +3,10 @@ define([], function close_mobile_nav() {
 	close_button.onclick = function(event) {
 		var mobile_nav_bar = document.body.getElementsByClassName('mobile-nav-bar')[0];
 		var hamburger_button = document.body.getElementsByClassName('nav-button')[0];
-		var mobile_header = document.body.getElementsByClassName('mobile-header')[0];
-		var mobile_logo = document.body.getElementsByClassName('mobile-logo')[0];
-		mobile_logo.style.display="";
-		mobile_header.style.borderBottom="";
+		//var mobile_header = document.body.getElementsByClassName('mobile-header')[0];
+		//var mobile_logo = document.body.getElementsByClassName('mobile-logo')[0];
+		//mobile_logo.style.display="";
+		//mobile_header.style.borderBottom="";
 		mobile_nav_bar.style.display = "";
 		hamburger_button.style.display = "block";
 	}
